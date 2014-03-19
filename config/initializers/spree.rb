d77562f2571611d9f1567648f97dd37d31978a4c
Alex Lyashenko <rubydeve@gmail.com>
2014-03-19 22:40:09 +0200

    setup S3

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,16 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  config.use_s3 = true
+  config.s3_bucket = 'coffeeholics'
+  config.s3_access_key = ENV['AWS_ACCESS_KEY_ID']
+  config.s3_secret = ENV['AWS_SECRET_ACCESS_KEY']
+  config.attachment_url = ':s3_eu_url'
+  config.s3_host_alias = ENV['AWS_REGION']
+end
+
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+  "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
 end
 
 Spree.user_class = "Spree::User"
